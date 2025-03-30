55e251271bb96eaaec211285613eb50e843f1d8a
BariKunal <kunalbari751@gmail.com>
2025-03-30 23:21:30 +0530

    new changes

--- StoteManagement_Collection_App/src/ClientApp/ClientApplication.java
+++ StoteManagement_Collection_App/src/ClientApp/ClientApplication.java
@@ -3,7 +3,6 @@ package ClientApp;
 import Service.ProductServiceImpl;
 
 public class ClientApplication {
-
 	static ProductServiceImpl prodService = new ProductServiceImpl();
 	public static void main(String[] args) {
 		
