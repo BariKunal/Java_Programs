2cd062330833dabc2194db1b3882a6a804184726
BariKunal <kunalbari751@gmail.com>
2025-04-13 23:53:24 +0530

    common elements in two array

---
+++ String_Array_Loops/src/Arrays/Common_Ele_Two_array.java
@@ -0,0 +1,27 @@
+package Arrays;
+import java.util.*;
+public class Common_Ele_Two_array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter The Array Size :");
+		int size = sc.nextInt();
+		int []a = new int[size];
+		int []b = new int[size];
+		System.out.println("Enter the 1st Array Elements :");
+		for(int i=0; i<size; i++) {
+			a[i] = sc.nextInt();
+		}
+		
+		System.out.println("Enter the 2nd Array Elements :");
+		for(int i=0; i<size; i++) {
+			b[i] = sc.nextInt();
+		}
+		
+		for(int i=0; i<size; i++) {
+			
+		}
+	}
+
+}
