8d02398776ae6e839fe58ea26422429a8052007f
BariKunal <kunalbari751@gmail.com>
2025-04-15 22:12:50 +0530

    Duplicates elements properly work with 3 duplicate values

--- String_Array_Loops/src/Arrays/Duplicate_Ele_Array.java
+++ String_Array_Loops/src/Arrays/Duplicate_Ele_Array.java
@@ -6,22 +6,34 @@ public class Duplicate_Ele_Array {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner (System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter The Array Size :");
 		int size = sc.nextInt();
 		int a[] = new int[size];
-//		boolean flag=true;
-		
+
 		System.out.print("Enter The Array Elements :");
-		for(int i=0; i<size; i++) {
+		for (int i = 0; i < size; i++) {
 			a[i] = sc.nextInt();
 		}
+		
 		System.out.println("Duplicates Elements In Array :");
-		for(int i=0; i<size; i++) {
-			for(int j=i+1; j<size; j++) {
-				if(a[i]==a[j]) {
-					System.out.print(a[i]+" ");		
-					}
+		
+		for (int i = 0; i < size; i++) {
+			if (a[i] == -1) {
+				continue;
+			}
+			
+			boolean flag = false;
+			
+			for (int j = i + 1; j < size; j++) {
+				if (a[i] == a[j]) {
+					flag = true;
+					a[j] = -1;
+				}
+			}
+			if (flag) {
+				System.out.print(a[i] + " ");
+				a[i] = -1;
 			}
 		}
 	}
