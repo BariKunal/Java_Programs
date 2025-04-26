70b150ce2bbf568a60b3d32c8723ebf5144a10bc
BariKunal <kunalbari751@gmail.com>
2025-04-26 23:40:24 +0530

    Dynamic right angle pattern in Java

---
+++ String_Array_Loops/src/Looping/Pattern4.java
@@ -0,0 +1,23 @@
+package Looping;
+
+import java.util.Scanner;
+
+public class Pattern4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter The Number :");
+		int num = sc.nextInt();
+		
+		for(int i=1; i<=num; i++) {
+			for(int j=1; j<=num; j++) {
+				if(i>=j) {
+					System.out.print("* ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
