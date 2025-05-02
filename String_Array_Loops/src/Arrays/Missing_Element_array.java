21ab5faecd5b886f06778d446fba25ba71ef29ea
BariKunal <kunalbari751@gmail.com>
2025-05-02 22:25:11 +0530

    Missing Element In Array

--- String_Array_Loops/src/Arrays/Missing_Element_array.java
+++ String_Array_Loops/src/Arrays/Missing_Element_array.java
@@ -6,32 +6,32 @@ public class Missing_Element_array {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc =new Scanner (System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter The array size :");
 		int size = sc.nextInt();
-		
-		int [] a = new int [size];
+
+		int[] a = new int[size];
 		System.out.println("Enter the array elements :");
-		for(int i=0; i<size; i++) {
+		for (int i = 0; i < size; i++) {
 			a[i] = sc.nextInt();
 		}
-		
-		for(int i=0; i<size-1; i++) {				//sorting of array if not sorted..
-			for(int j=i+1; j<size; j++) {
-				if(a[i]>a[j]) {
+
+		for (int i = 0; i < size; i++) { // sorting of array if not sorted..
+			for (int j = i + 1; j < size; j++) {
+				if (a[i] > a[j]) {
 					int temp = a[i];
-						a[i] = a[j];
-						a[j] = temp;
- 				}
+					a[i] = a[j];
+					a[j] = temp;
+				}
 			}
 		}
-		
+
 		System.out.print("Missing Elements :");
-		for(int i=0; i<size-1; i++) {
-			for(int j=a[i]+1; j<a[i+1]; j++) {
-				
-					System.out.print(j+" ");
-				
+		for (int i = 0; i < size - 1; i++) {
+			for (int j = a[i] + 1; j < a[i + 1]; j++) {
+
+				System.out.print(j + " ");
+
 			}
 		}
 	}
