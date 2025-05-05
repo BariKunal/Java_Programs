bce1e20de5f51302f270ab4fc914bff8cad852df
BariKunal <kunalbari751@gmail.com>
2025-05-05 22:43:30 +0530

    Count the words in string

---
+++ String_Array_Loops/src/May_Test_Answers/Test13CountWords.java
@@ -0,0 +1,30 @@
+package May_Test_Answers;
+
+import java.util.*;
+public class Test13CountWords
+{
+	public static void main(String x[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter The String :");
+		String str = sc.nextLine();
+		int count = 0;
+		boolean flag = false;
+		
+		for(int i=0; i<str.length(); i++)
+		{
+			char ch = str.charAt(i);
+			if(ch != ' ' && !flag)
+			{
+				count++;
+				flag = true;
+			}
+			else if(ch == ' ') {
+				flag = false;
+			}
+		}
+		if(count>0)
+		System.out.println("Count Is 1st: "+count);
+		else System.out.println("Not World Present...");
+	}
+}
